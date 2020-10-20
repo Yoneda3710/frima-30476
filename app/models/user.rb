115e85e26cb6393142cc8b62f2b268feb93697fa
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-21 06:27:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :nickname,            presence: true
+  validates :email,               presence: true#, uniqueness: true, format: { with: /\A[a-z\d]@[a-z\d]\z/ }
+  validates :password,            presence: true, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }, confirmation: true
+  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
+  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
+  validates :kana_family_name,    presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
+  validates :kana_first_name,     presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
+  validates :birthday,            presence: true
+
 end

c84b3ba1398b66b05cdd22b60195d956d9fa22da
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-21 08:59:27 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :nickname,            presence: true
-  #validates :email,               #uniqueness: true
+  validates :email,               uniqueness: true
   validates :password,            format: { with: PASSWORD_REGEX }, length: { minimum: 6 }, confirmation: true
   validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
   validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
   validates :kana_family_name,    presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
   validates :kana_first_name,     presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
   validates :birthday,            presence: true
-
+  validates :email, uniqueness: true
 end

cb6106924c2e8161beac3e9c8ca3cda55b921f11
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-21 18:24:36 +0900

    商品出品ページcss設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,30 @@
 class ItemsController < ApplicationController
 
   def index
+    
+  end
+
+  def new
+    @item = Item.new
+  end
+   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image, :itemcategory_id, :itemsalesstatus_id, :itemshipping_id, :itemprefecture_id, :itemscheduled_id,).merge(user_id: current_user.id)
   end
   
 end

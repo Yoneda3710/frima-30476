367ebf69564faac4836c194528505ed7e3334452
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-21 16:03:29 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,21 @@ class User < ApplicationRecord
 
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :nickname,            presence: true
-  validates :email,               uniqueness: true
-  validates :password,            format: { with: PASSWORD_REGEX }, length: { minimum: 6 }, confirmation: true
-  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
-  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
-  validates :kana_family_name,    presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
-  validates :kana_first_name,     presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/ }
-  validates :birthday,            presence: true
-  validates :email, uniqueness: true
+  NAME_REGEX = /\A[ぁ-んァ-ンー-龥]+\z/.freeze
+  KANA_NAME_REGEX = /\A[ア-ン゛゜ァ-ォャ-ョー]+\z/.freeze
+  with_options presence: true do
+   validates :nickname
+   validates :birthday
+   validates :email,               uniqueness: true
+   validates :password,            format: { with: PASSWORD_REGEX }, length: { minimum: 6 }, confirmation: true
+
+   with_options format:{ with: NAME_REGEX } do
+    validates :family_name
+    validates :first_name
+   end
+   with_options format:{ with: KANA_NAME_REGEX } do
+    validates :kana_family_name
+    validates :kana_first_name
+   end
+  end
 end

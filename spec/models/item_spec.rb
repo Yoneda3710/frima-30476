dabaf659e3eb69be428c2b6b10f56f544b9e58af
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-22 21:21:01 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "カテゴリーが空だと保存されない" do
-        @item.category_id ="1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "商品の状態が空だと保存されない" do
-        @item.status_id ="1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it "配送料負担の情報が空だと保存されない" do
-        @item.shipping_id ="1"
+        @item.shipping_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping must be other than 1")
       end
       it "発送元が空だと保存されない" do
-        @item.prefecture_id ="1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it "発送までの日数が空だと保存されない" do
-        @item.scheduled_id ="1"
+        @item.scheduled_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled must be other than 1")
       end

246787b46b8756d9067bda37ad2171f8d5bc8d57
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-22 18:35:17 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,17 +58,17 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "価格が￥299以下だと保存されない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 300")
       end
       it "価格が￥10000000以上だと保存されない" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
       it "価格が半角数字以外だと保存されない" do
-        @item.price ="３００００"
+        @item.price =３００００
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end

32372dbe6827c9ac8226c69520444b450c38a4e0
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-23 16:45:42 +0900

    問題の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item,           only: [:show, :destroy]
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -19,12 +19,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
   
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to action: :index
     end
   end
@@ -33,6 +33,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :price, :image, :category_id, :status_id, :shipping_id, :prefecture_id, :scheduled_id,).merge(user_id: current_user.id)
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
   
 end

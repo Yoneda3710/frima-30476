bd48819234bf8f2f192cd2d0b06f0b543ff317a8
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-24 21:00:58 +0900

    cssの再配置

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,20 @@
 class OrdersController < ApplicationController
+
+  def index
+   @order = Item.find(params[:item_id])
+  end
+  
+  def create
+    Order.create(order_params)
+    Address.create(address_params)
+    redirect_to action::index
+  end
+
+  private
+  def order_params
+    params.permit.merge(user_id: user.id, item_id: item.id)
+  end
+
+
+
 end
